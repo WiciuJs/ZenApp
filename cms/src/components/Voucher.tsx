785772dd611419255922ff872861eec591d6580e
Witold Janus <witek.janus@int.pl>
2023-09-17 21:33:30 +0200

    Update Voucher

--- cms/src/components/Voucher.tsx
+++ cms/src/components/Voucher.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Document, Page, Text, View, StyleSheet, PDFViewer } from '@react-pdf/renderer';
 import html2canvas from 'html2canvas';
 import jsPDF from 'jspdf';
-import '../styles/Voucher.scss'; 
+import '../styles/Voucher.scss';
 
 const Voucher = () => {
   const [formData, setFormData] = useState({
@@ -21,10 +21,10 @@ const Voucher = () => {
     const input = document.getElementById('voucher-content');
 
     if (input) {
-      html2canvas(input).then((canvas) => {
+      html2canvas(input, { scale: 2 }).then((canvas) => {
         const imgData = canvas.toDataURL('image/png');
-        const pdf = new jsPDF('p', 'mm', 'a4');
-        pdf.addImage(imgData, 'PNG', 0, 0, 210, 297);
+        const pdf = new jsPDF('portrait', 'mm', 'a5');
+        pdf.addImage(imgData, 'PNG', 0, 0, 148, 210);
         pdf.save('voucher.pdf');
       });
     }
@@ -32,60 +32,62 @@ const Voucher = () => {
 
   return (
     <div className="voucher-container">
-      <h1 className="voucher-header">Voucher Generator</h1>
-      <form className="voucher-form">
-        <label className="voucher-label" htmlFor="firstName">
-          Imię:
-          <input
-            type="text"
-            id="firstName"
-            name="firstName"
-            className="voucher-input"
-            onChange={handleInputChange}
-          />
-        </label>
-        <label className="voucher-label" htmlFor="lastName">
-          Nazwisko:
-          <input
-            type="text"
-            id="lastName"
-            name="lastName"
-            className="voucher-input"
-            onChange={handleInputChange}
-          />
-        </label>
-        <label className="voucher-label" htmlFor="massageType">
-          Rodzaj masażu:
-          <input
-            type="text"
-            id="massageType"
-            name="massageType"
-            className="voucher-input"
-            onChange={handleInputChange}
-          />
-        </label>
-        <label className="voucher-label" htmlFor="date">
-          Data:
-          <input
-            type="date"
-            id="date"
-            name="date"
-            className="voucher-input"
-            onChange={handleInputChange}
-          />
-        </label>
-      </form>
-      <button className="voucher-button" onClick={generatePDF}>
-        Generuj PDF
-      </button>
-      <div id="voucher-content" className="voucher-pdf-preview">
-        <PDFViewer width={400} height={400}>
+      <div className="voucher-form-container">
+        <h1 className="voucher-header">Voucher Generator</h1>
+        <form className="voucher-form">
+          <label className="voucher-label" htmlFor="firstName">
+            Imię:
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              className="voucher-input"
+              onChange={handleInputChange}
+            />
+          </label>
+          <label className="voucher-label" htmlFor="lastName">
+            Nazwisko:
+            <input
+              type="text"
+              id="lastName"
+              name="lastName"
+              className="voucher-input"
+              onChange={handleInputChange}
+            />
+          </label>
+          <label className="voucher-label" htmlFor="massageType">
+            Rodzaj masażu:
+            <input
+              type="text"
+              id="massageType"
+              name="massageType"
+              className="voucher-input"
+              onChange={handleInputChange}
+            />
+          </label>
+          <label className="voucher-label" htmlFor="date">
+            Data:
+            <input
+              type="date"
+              id="date"
+              name="date"
+              className="voucher-input"
+              onChange={handleInputChange}
+            />
+          </label>
+          <button className="voucher-button" onClick={generatePDF}>
+            Generuj PDF
+          </button>
+        </form>
+      </div>
+      <div className="voucher-pdf-preview" id="voucher-content">
+        <PDFViewer width="100%" height="100%">
           <Document>
-            <Page size="A4" style={styles.page}>
+            <Page size="A5" style={styles.page}>
               <View style={styles.section}>
                 <Text>{formData.firstName} {formData.lastName}</Text>
-                <Text>Rodzaj masażu: {formData.massageType}</Text>
-                <Text>Data: {formData.date}</Text>
+                <Text>{formData.massageType}</Text>
+                <Text>{formData.date}</Text>
               </View>
             </Page>
           </Document>
@@ -98,7 +100,7 @@ const Voucher = () => {
 const styles = StyleSheet.create({
   page: {
     flexDirection: 'row',
-    backgroundColor: 'white',
+    backgroundColor: 'transparent', 
   },
   section: {
     margin: 10,
@@ -108,3 +110,5 @@ const styles = StyleSheet.create({
 });
 
 export default Voucher;
+
+
