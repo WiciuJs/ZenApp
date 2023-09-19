e97750f863fca38202b0f499b3455de395103126
Witold Janus <witek.janus@int.pl>
2023-09-19 21:35:58 +0200

    Update

--- cms/src/components/Navigation.tsx
+++ cms/src/components/Navigation.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import '../styles/Navigation.scss';
-import '../styles/Calendar.scss';
-import Voucher from './Voucher';
-import MyCalendar from './Calendar'; 
+import React, { useState } from "react";
+import "../styles/Navigation.scss";
+import "../styles/Calendar.scss";
+import Voucher from "./Voucher";
+import CalendarView from "../views/CalendarView";
 
 function Navigation() {
   const [showCalendar, setShowCalendar] = useState(false);
@@ -10,25 +10,30 @@ function Navigation() {
 
   const handleCalendarClick = () => {
     setShowCalendar(true);
-    setShowVoucher(false); 
+    setShowVoucher(false);
   };
 
   const handleVoucherClick = () => {
     setShowVoucher(true);
-    setShowCalendar(false); 
+    setShowCalendar(false);
   };
 
   return (
     <div>
       <div className="Navigation">
-        <div className="Navigation-item" onClick={handleCalendarClick}>Kalendarz </div>
+        <div className="Navigation-item" onClick={handleCalendarClick}>
+          Kalendarz{" "}
+        </div>
         <div className="Navigation-item">Lista klientów</div>
         <div className="Navigation-item">Zaopatrzenie</div>
-        <div className="Navigation-item" onClick={handleVoucherClick}> Voucher </div>
+        <div className="Navigation-item" onClick={handleVoucherClick}>
+          {" "}
+          Voucher{" "}
+        </div>
         <div className="Navigation-item">Zabiegi</div>
       </div>
 
-      {showCalendar && <MyCalendar />} 
+      {showCalendar && <CalendarView />}
       {showVoucher && <Voucher />}
     </div>
   );
