bb1a9b74d01a345477287cc7d3b2a33e186bb274
Witold Janus <witek.janus@int.pl>
2023-09-21 20:11:46 +0200

    Add Action

--- cms/src/views/CustomerListView.tsx
+++ cms/src/views/CustomerListView.tsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import UserForm from '../components/CustomerForm';
-import '../styles/CustomerViews.scss'; 
+import '../styles/CustomerViews.scss';
+import { faUserXmark as faSolidUserXmark, faBookmark as faDuotoneBookmark, faUserGear as faDuotoneUserGear } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 interface Customer {
   _id: string;
@@ -14,12 +16,14 @@ interface Customer {
 }
 
 const CustomerListView: React.FC = () => {
-  const [customer, setCustomer] = useState<Customer[]>([]);
+  const [customers, setCustomers] = useState<Customer[]>([]);
+  const [selectedCustomer, setSelectedCustomer] = useState<Customer | null>(null);
+  const [isEditMode, setIsEditMode] = useState<boolean>(false);
 
   useEffect(() => {
     axios.get('http://127.0.0.1:5000/api/customer')
       .then((res) => {
-        setCustomer(res.data);
+        setCustomers(res.data);
       })
       .catch((error) => {
         console.error('Błąd podczas pobierania użytkowników:', error);
@@ -27,15 +31,44 @@ const CustomerListView: React.FC = () => {
   }, []);
 
   const handleUserAdded = (newCustomer: Customer) => {
-    setCustomer([...customer, newCustomer]);
+    setCustomers([...customers, newCustomer]);
+  };
+
+  const handleDeleteCustomer = (customerId: string) => {
+    axios.delete(`http://127.0.0.1:5000/api/customer/${customerId}`)
+      .then(() => {
+        setCustomers(customers.filter(customer => customer._id !== customerId));
+      })
+      .catch((error) => {
+        console.error('Błąd podczas usuwania użytkownika:', error);
+      });
+  };
+
+  const handleEditCustomer = (customer: Customer) => {
+    setSelectedCustomer(customer);
+    setIsEditMode(true);
+  };
+
+  const handleSaveCustomer = (updatedCustomer: Customer) => {
+    axios.put(`http://127.0.0.1:5000/api/customer/${updatedCustomer._id}`, updatedCustomer)
+      .then(() => {
+        setCustomers(customers.map(customer =>
+          customer._id === updatedCustomer._id ? updatedCustomer : customer
+        ));
+        setSelectedCustomer(null);
+        setIsEditMode(false);
+      })
+      .catch((error) => {
+        console.error('Błąd podczas aktualizacji użytkownika:', error);
+      });
   };
 
   return (
-    <div className="customer-view"> 
-      <div className="customer-form"> 
+    <div className="customer-view">
+      <div className="customer-form">
         <UserForm onCustomerAdded={handleUserAdded} />
       </div>
-      <div className="customer-list"> 
+      <div className="customer-list">
         <h2>Lista użytkowników</h2>
         <table>
           <thead>
@@ -46,10 +79,11 @@ const CustomerListView: React.FC = () => {
               <th>E-mail</th>
               <th>Numer telefonu</th>
               <th>Komentarze</th>
+              <th>Akcje</th>
             </tr>
           </thead>
           <tbody>
-            {customer.map((customer) => (
+            {customers.map((customer) => (
               <tr key={customer._id}>
                 <td>{customer.name}</td>
                 <td>{customer.surname}</td>
@@ -57,10 +91,29 @@ const CustomerListView: React.FC = () => {
                 <td>{customer.mail}</td>
                 <td>{customer.phoneNumber}</td>
                 <td>{customer.comments}</td>
+                <td>
+                  <button className="neon-button-action-edit" onClick={() => handleEditCustomer(customer)}>
+                    <FontAwesomeIcon icon={faDuotoneUserGear} />
+                  </button>
+                  <button className="neon-button-action-delete" onClick={() => handleDeleteCustomer(customer._id)}>
+                    <FontAwesomeIcon icon={faSolidUserXmark} />
+                  </button>
+                  <button className="neon-button-action" onClick={() => handleSaveCustomer(customer)}>
+                    <FontAwesomeIcon icon={faDuotoneBookmark} />
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
         </table>
+        {isEditMode && selectedCustomer && (
+          <div className="edit-form">
+            <h3>Edytuj użytkownika</h3>
+            <UserForm
+              onCustomerAdded={handleSaveCustomer}
+            />
+          </div>
+        )}
       </div>
     </div>
   );
