96243b6dfbbe07b23b36324693b9ef05f67b6acb
Witold Janus <witek.janus@int.pl>
2023-09-26 22:15:43 +0200

    Logowanie Fail-1

--- BackEnd/src/controllers/authController.ts
+++ BackEnd/src/controllers/authController.ts
@@ -1,25 +1,26 @@
 import { Request, Response } from 'express'; 
-
+import User from '../models/User';
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+
 
 class AuthController {
   async register(req: Request, res: Response) { 
     try {
       const { username, password } = req.body;
-
+  
       const existingUser = await User.findOne({ username });
       if (existingUser) {
         return res.status(400).json({ message: 'Użytkownik o tej nazwie już istnieje' });
       }
-
+  
       const hashedPassword = await bcrypt.hash(password, 10);
       const newUser = new User({ username, password: hashedPassword });
       await newUser.save();
-
+  
       res.status(201).json({ message: 'Rejestracja udana' });
     } catch (error) {
+      console.error('Błąd rejestracji:', error); 
       res.status(500).json({ message: 'Wystąpił błąd podczas rejestracji' });
     }
   }
