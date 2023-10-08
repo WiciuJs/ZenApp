376f83f9bcbd82f63234ebc3653374791c6dbde7
Witold Janus <witek.janus@int.pl>
2023-10-08 16:55:01 +0200

    .

--- BackEnd/src/routes/authRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-const authController = require('../controllers/authController');
-const router = Router();
-
-router.post('/register', authController.register);
-router.post('/login', authController.login);
-
-export default router;
