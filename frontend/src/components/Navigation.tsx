fd61accca417fb5403345f95b2c9cba120ff35e4
Witold Janus <witek.janus@int.pl>
2023-10-17 21:41:06 +0200

    Update Nav

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -1,15 +1,7 @@
 import { Link, useLocation } from 'react-router-dom';
 import styles from '../styles/Navigation.module.scss';
+import { faCalendar, faAddressBook, faBox, faGift, faSpa, faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faCalendar,
-  faAddressBook,
-  faBox,
-  faGift,
-  faSpa,
-  faRightFromBracket
-} from '@fortawesome/free-solid-svg-icons';
-
 interface NavigationProps {
   user: { username: string } | null;
   logout: () => void;
@@ -19,58 +11,25 @@ const Navigation: React.FC<NavigationProps> = ({ user, logout }) => {
   const location = useLocation();
 
   return (
-    <nav className={`navbar navbar-expand-lg navbar-light ${styles.navbar}`}>
-      <div className={`container-fluid ${styles.containerFluid}`}>
-        <ul className={`navbar-nav ${styles.navbarNav}`}>
-          <li className={`nav-item ${styles.navItem}`}>
-            <Link
-              className={`nav-link ${location.pathname === '/calendar' ? 'active' : ''}`}
-              to="/calendar"
-            >
-              <FontAwesomeIcon icon={faCalendar} />
-            </Link>
-          </li>
-          <li className={`nav-item ${styles.navItem}`}>
-            <Link
-              className={`nav-link ${location.pathname === '/client-list' ? 'active' : ''}`}
-              to="/client-list"
-            >
-              <FontAwesomeIcon icon={faAddressBook} />
-            </Link>
-          </li>
-          <li className={`nav-item ${styles.navItem}`}>
-            <Link
-              className={`nav-link ${location.pathname === '/supplies' ? 'active' : ''}`}
-              to="/supplies"
-            >
-              <FontAwesomeIcon icon={faBox} />
-            </Link>
-          </li>
-          <li className={`nav-item ${styles.navItem}`}>
-            <Link
-              className={`nav-link ${location.pathname === '/voucher' ? 'active' : ''}`}
-              to="/voucher"
-            >
-              <FontAwesomeIcon icon={faGift} />
-            </Link>
-          </li>
-          <li className={`nav-item ${styles.navItem}`}>
-            <Link
-              className={`nav-link ${location.pathname === '/treatments' ? 'active' : ''}`}
-              to="/treatments"
-            >
-              <FontAwesomeIcon icon={faSpa} />
-            </Link>
-          </li>
-          <li className={`nav-item ${styles.navItem}`}>
-            <a
-              href="#"
-              className={`nav-link ${styles.navLink}`}
-              onClick={logout}
-            >
-              <FontAwesomeIcon icon={faRightFromBracket} />
-            </a>
-          </li>
+    <nav className={`${styles.navbar}`}>
+      <div className={`${styles.containerFluid}`}>
+        <ul className={`${styles.navbarNav}`}>
+          {[
+            { path: '/calendar', icon: faCalendar },
+            { path: '/client-list', icon: faAddressBook },
+            { path: '/supplies', icon: faBox },
+            { path: '/voucher', icon: faGift },
+            { path: '/treatments', icon: faSpa },
+            { path: '#', icon: faRightFromBracket, onClick: logout }
+          ].map(item => (
+            <li key={item.path} className={`${styles.navItem} ${location.pathname === item.path ? styles.active : ''}`}>
+              <Link to={item.path} onClick={item.onClick}>
+                <div className={`${styles.iconContainer}`}>
+                  <FontAwesomeIcon className={`${styles.navIcon}`} icon={item.icon} />
+                </div>
+              </Link>
+            </li>
+          ))}
         </ul>
       </div>
     </nav>
